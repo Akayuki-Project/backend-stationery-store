8e0b83b69dc03d8b2fc606f46ae011f483eb4fec
Akayuki Kobayashi <148461661+Akayuki-Project@users.noreply.github.com>
2025-04-14 20:02:33 +0700

    Update TransactionController.js

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -45,7 +45,7 @@ exports.createTransaction = async (req, res) => {
     // Kirim response ke frontend
     res.status(201).json({
       message: "Transaksi berhasil dibuat",
-      snap_token: snapToken,
+      snap_token: snapToken.midtrans_url,
       transaction_id: order_id,
     });
   } catch (err) {
