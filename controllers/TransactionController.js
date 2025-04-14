7ca8d622cdc8c91d62e6420ca2e6ffa025eabf85
Akayuki Kobayashi <148461661+Akayuki-Project@users.noreply.github.com>
2025-04-14 20:25:24 +0700

    Update TransactionController.js

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -30,8 +30,7 @@ exports.createTransaction = async (req, res) => {
 
     // Dapatkan Snap Token, bukan URL
     const transaction = await snap.createTransaction(parameter);
-    const snapToken = transaction.token;
-    const snapToken = transaction.redirect_url;
+    const snapToken = transaction.token.redirect_url;
 
     // Simpan ke database
     const newTransaction = new Transaction({
